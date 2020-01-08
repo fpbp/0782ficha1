2979807072d147f796bbbe75ee1f4ca878da01ef
fpbp <58472324+fpbp@users.noreply.github.com>
2020-01-08 11:15:59 -0100

    Add files via upload

---
+++ Prog4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main (void){
+    char nome[200];
+    float primeiroteste;
+    float segundoteste;
+    float terceiroteste;
+    float mediaprimeiro;
+    float mediasegundo;
+    float mediaterceiro;
+    float mediaponderada;
+
+    printf("Aluno: \n");
+    scanf("%s", &nome);
+
+    printf("nota1: \n");
+    scanf("%d" &primeiroteste);
+
+    printf("nota2: \n");
+    scanf("%d" &segundoteste);
+
+    printf("nota3: \n");
+    scanf("%d" &terceiroteste);
+
+    mediaprimeiro = primeiroteste * 0.25;
+    mediasegundo = segundoteste * 0.35;
+    mediaterceiro = terceiroteste * 0.40;
+
+    mediaponderada = mediaprimeiro + mediasegundo
+
+    printf("sua media ponderada é %f");
+
+    
+
+
+
+
+
+
+
+
+
+}
